3d639fc0ee8e4ddf2b90282a16e54a856f8bb978
ogabek <quvvatullayev@gmail.com>
2023-06-06 15:07:52 +0500

    create education

---
+++ my_portfolio/views/education.py
@@ -0,0 +1,31 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from ..models import (
+    Education,
+)
+from ..serializer import(
+    EducationSerializer,
+)
+
+class Add_education(APIView):
+    def post(self, request:Request):
+        data = request.data
+        serializer = EducationSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+class Education_list(APIView):
+    def get(self, request:Request):
+        education = Education.objects.all()
+        serializer = EducationSerializer(education, many=True)
+        return Response(serializer.data)
+    
