01f76673f83168c28231bbf57d154aa6081d946c
ogabek <quvvatullayev@gmail.com>
2023-06-06 14:44:16 +0500

    add admin

--- my_portfolio/admin.py
+++ my_portfolio/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import (
+    About_me,
+    Skills,
+    Education,
+    Experience,
+    Projects,
+)
 
-# Register your models here.
+admin.site.site_header = 'My Portfolio Admin Panel'
+admin.site.site_title = 'My Portfolio Admin Panel'
+admin.site.index_title = 'My Portfolio Admin Panel'
+
+@admin.register(About_me)
+class About_meAdmin(admin.ModelAdmin):
+    list_display = ['name', 'surname', 'age', 'email', 'phone', 'address', 'github', 'linkedin', 'facebook', 'instagram', 'telegram', 'photo', 'description']
+    fields = ['name', 'surname', 'age', 'email', 'phone', 'address', 'github', 'linkedin', 'facebook', 'instagram', 'telegram', 'photo', 'description']
+    # exclude = ['name', 'surname', 'age', 'email', 'phone', 'address', 'github', 'linkedin', 'facebook', 'instagram', 'telegram', 'photo', 'description']
+
+@admin.register(Skills)
+class SkillsAdmin(admin.ModelAdmin):
+    list_display = ['skill_name', 'skill_icon', 'skill_description', 'skill_document']
+    fields = ['skill_name', 'skill_icon', 'skill_description', 'skill_document']
+    # exclude = ['skill_name', 'skill_icon', 'skill_description', 'skill_document']
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ['education_name', 'education_icon', 'education_description']
+    fields = ['education_name', 'education_icon', 'education_description']
+    # exclude = ['education_name', 'education_icon', 'education_description']
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ['experience_name', 'experience_description']
+    fields = ['experience_name', 'experience_description']
+
+@admin.register(Projects)
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display = ['project_name', 'project_description', 'project_image', 'project_link', 'project_github', 'about_me']
+    fields = ['project_name', 'project_description', 'project_image', 'project_link', 'project_github', 'about_me']
+    # exclude = ['project_name', 'project_description', 'project_image', 'project_link', 'project_github', 'about_me']
--- my_portfolio/serializer.py
+++ my_portfolio/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from models import (
+from .models import (
     About_me,
     Skills,
     Education,
