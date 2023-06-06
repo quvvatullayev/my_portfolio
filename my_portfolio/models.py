5093009f95967f3d67eefcf267c63dfb3c3fe7f1
ogabek <quvvatullayev@gmail.com>
2023-06-06 14:20:50 +0500

    create models all

--- my_portfolio/models.py
+++ my_portfolio/models.py
@@ -23,8 +23,32 @@ class Skills(models.Model):
     skill_icon = models.ImageField(upload_to='images/')
     skill_description = models.TextField()
     skill_document = models.TextField()
-    about_me = models.ForeignKey(About_me, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.skill_name
 
+class Education(models.Model):
+    education_name = models.CharField(max_length=100)
+    education_icon = models.ImageField(upload_to='images/')
+    education_description = models.TextField()
+
+    def __str__(self):
+        return self.education_name
+    
+class Experience(models.Model):
+    experience_name = models.CharField(max_length=100)
+    experience_description = models.TextField()
+
+    def __str__(self):
+        return self.experience_name
+    
+class Projects(models.Model):
+    project_name = models.CharField(max_length=100)
+    project_description = models.TextField()
+    project_image = models.ImageField(upload_to='images/')
+    project_link = models.CharField(max_length=100)
+    project_github = models.CharField(max_length=100)
+    about_me = models.ForeignKey(About_me, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.project_name
