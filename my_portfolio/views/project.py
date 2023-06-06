4f034d4b80a019adb26bf5c20faa635532101a1e
ogabek <quvvatullayev@gmail.com>
2023-06-06 20:05:59 +0500

    create all

--- my_portfolio/views/project.py
+++ my_portfolio/views/project.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
+from django.shortcuts import render
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
@@ -30,4 +31,7 @@ class Projects_list(APIView):
     def get(self, request:Request):
         projects = Projects.objects.all()
         serializer = ProjectsSerializer(projects, many=True)
-        return Response(serializer.data)
+        data = {
+            "projects": serializer.data,
+        }
+        return render(request, "projects.html", context=data)
